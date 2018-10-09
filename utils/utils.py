fix average class

utils/utils.py
@@ -45,13 +45,13 @@ class AverageMeter(object):
         self.total_sum += val * n
         self.total_count += n
         if self.total_count>10:
-            self.total_avg = np.mean(np.array(self.array[-10:]))#self.total_sum / self.total_count
+            self.total_avg = np.median(np.array(self.array[-10:]))#self.total_sum / self.total_count
         else:
             self.total_avg = self.total_sum / self.total_count
 
         self.sum += val * n
         self.count += n
-        self.avg = self.sum / self.count
+        self.avg = self.array[-1]
     
     def load(self,narray,n=1):
         for val in narray: