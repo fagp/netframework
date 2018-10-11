1ca2c99dc104f8650956bd6b9b884e74820170b0
Fidel Guerrero <fillo8985@gmail.com>
2018-10-11 16:41:22 -0300

    smoother plot

--- utils/utils.py
+++ utils/utils.py
@@ -44,8 +44,8 @@ class AverageMeter(object):
         
         self.total_sum += val * n
         self.total_count += n
-        if self.total_count>10:
-            self.total_avg = np.median(np.array(self.array[-10:]))#self.total_sum / self.total_count
+        if self.total_count>50:
+            self.total_avg = np.median(np.array(self.array[-50:]))#self.total_sum / self.total_count
         else:
             self.total_avg = self.total_sum / self.total_count
 
