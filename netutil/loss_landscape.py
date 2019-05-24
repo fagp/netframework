fixed normalization loss land

netutil/loss_landscape.py
@@ -39,6 +39,7 @@ class LandscapeVis():
         parser = argparse.ArgumentParser(description='Landscape arguments description')
         parser.add_argument('--experiment', nargs='?', type=str, default='experiment', help='Experiment name')
         parser.add_argument('--surface_name', nargs='?', type=str, default='surface', help='Output surface file name')
+        parser.add_argument('--plane', nargs='?', type=str, default='', help='Path to folder containing plane files')
 
         parser.add_argument('--visdom', action='store_true', help='If included shows visdom visulaization')
         parser.add_argument('--show_rate', nargs='?',type=int, default=4, help='Visdom show after num of iterations (used with --visdom)')
@@ -46,6 +47,7 @@ class LandscapeVis():
         parser.add_argument('--save_rate', nargs='?',type=int, default=10, help='Save after num of iterations (if --save_rate=0 then no save is done during training)')
 
         parser.add_argument('--use_cuda', nargs='?',type=int, default=0, help='GPU device (if --use_cuda=-1 then CPU used)')
+        parser.add_argument('--use_dataloader', nargs='?',type=int, default=0, help='Use dataloaders instead dataset iteration')
         parser.add_argument('--parallel', action='store_true', help='Use multiples GPU (used only if --use_cuda>-1)')
         parser.add_argument('--epoch', nargs='?', type=int, default=-1, help='Epoch number')
         parser.add_argument('--worker', nargs='?', type=int, default=0, help='Number of testing workers')
@@ -59,6 +61,7 @@ class LandscapeVis():
         
         parser.add_argument('--batch_size', nargs='?', type=int, default=-1, help='Minibatch size')
         parser.add_argument('--batch_acc', nargs='?', type=int, default=1, help='Minibatch accumulation')
+        parser.add_argument('--image_num', nargs='?', type=int, default=-1, help='Select image')
 
         parser.add_argument('--seed', nargs='?',type=int, default=123, help='Random seed (for reproducibility)')
         
@@ -68,7 +71,6 @@ class LandscapeVis():
         parser.add_argument('--ynorm', default='filter', help='direction normalization: filter | layer | weight')
         parser.add_argument('--xignore', default='biasbn', help='ignore bias and BN parameters: biasbn')
         parser.add_argument('--yignore', default='biasbn', help='ignore bias and BN parameters: biasbn')
-        parser.add_argument('--plane', nargs='?', type=str, default='', help='Path to folder containing plane files')
 
         args = parser.parse_args()
 
@@ -112,12 +114,12 @@ class LandscapeVis():
         self.visdom=args.visdom
         if self.visdom==True:
             self.vis = Visdom(use_incoming_socket=False)
-            self.vis.close(env='landscape_'+args.experiment)
-            self.visplotter = gph.VisdomLinePlotter(self.vis, env_name='landscape_'+args.experiment)
-            self.visheatmap = gph.HeatMapVisdom(self.vis, env_name='landscape_'+args.experiment)
-            self.visimshow  = gph.ImageVisdom(self.vis, env_name='landscape_'+args.experiment)
-            self.vistext    = gph.TextVisdom(self.vis, env_name='landscape_'+args.experiment)
-            self.vissurf    = gph.VisdomSurface(self.vis, env_name='landscape_'+args.experiment)
+            self.vis.close(env='landscape_'+args.experiment+'_'+args.surface_name)
+            self.visplotter = gph.VisdomLinePlotter(self.vis, env_name='landscape_'+args.experiment+'_'+args.surface_name)
+            self.visheatmap = gph.HeatMapVisdom(self.vis, env_name='landscape_'+args.experiment+'_'+args.surface_name)
+            self.visimshow  = gph.ImageVisdom(self.vis, env_name='landscape_'+args.experiment+'_'+args.surface_name)
+            self.vistext    = gph.TextVisdom(self.vis, env_name='landscape_'+args.experiment+'_'+args.surface_name)
+            self.vissurf    = gph.VisdomSurface(self.vis, env_name='landscape_'+args.experiment+'_'+args.surface_name)
 
         try:
             args.xmin, args.xmax, args.xnum = [float(a) for a in args.x.split(':')]
@@ -137,8 +139,10 @@ class LandscapeVis():
         self.show_rate = args.show_rate
         self.save_rate = args.save_rate
         self.surface_name=args.surface_name
+        self.use_dataloader=args.use_dataloader
         
         self.epochs=args.epoch
+        self.image_num=args.image_num
         self.folders=args.folders
         self.batch_size= expargs['batch_size'] if args.batch_size==-1 else args.batch_size
         self.dataset= expargs['dataset'] if args.dataset=='' else args.dataset
@@ -179,34 +183,25 @@ class LandscapeVis():
                                         experimentparams=expargs['modelparam'],
                                         config_file=os.path.join(defaults_path,'modelconfig.json'))
 
-        self.net.to(self.device)
         self.w = copy.deepcopy(self.net.state_dict())
 
-        netx=copy.deepcopy(self.net)
         if os.path.isfile(folders['plane_path_x']):
-            init_type=folders['plane_path_x']
+            self.wx=self.loadmodel(self.net,folders['plane_path_x'])
         else:
-            init_type='xavier_uniform'
-        init_params(netx,init_type)
-        if not os.path.isfile(folders['plane_path_x']):
-            normalize_directions_for_states(netx.state_dict(), self.net.state_dict(), args.xnorm, args.xignore)
-        self.wx=create_target_direction(self.net,netx)
-        if not os.path.isfile(folders['plane_path_x']):
-            self.cx, self.cy= 1, 0
-            self.savemodel(netx,folders['plane_path_x'])
+            states = self.net.state_dict()
+            self.wx=get_random_states(states)
+            normalize_directions_for_states(self.wx, self.net.state_dict(), args.xnorm, args.xignore)
+            self.savemodel(self.net,self.wx,folders['plane_path_x'])
 
-        nety=copy.deepcopy(self.net)
         if os.path.isfile(folders['plane_path_y']):
-            init_type=folders['plane_path_y']
+            self.wy=self.loadmodel(self.net,folders['plane_path_y'])
         else:
-            init_type='xavier_uniform'
-        init_params(nety,init_type)
-        if not os.path.isfile(folders['plane_path_y']):
-            normalize_directions_for_states(nety.state_dict(), self.net.state_dict(), args.ynorm, args.yignore)
-        self.wy=create_target_direction(self.net,nety)
-        if not os.path.isfile(folders['plane_path_y']):
-            self.cx, self.cy= 0, 1
-            self.savemodel(nety,folders['plane_path_y'])
+            states = self.net.state_dict()
+            self.wy=get_random_states(states)
+            normalize_directions_for_states(self.wy, self.net.state_dict(), args.ynorm, args.yignore)
+            self.savemodel(self.net,self.wy,folders['plane_path_y'])
+
+        self.net.to(self.device)
 
         self.cx, self.cy= 0, 0
 
@@ -265,7 +260,14 @@ class LandscapeVis():
                 coord=(self.combcoordinates[0][self.cx][self.cy] ,self.combcoordinates[1][self.cx][self.cy])
                 #code for checking if exist
                 set_states(self.net.module if self.use_parallel else self.net,self.w,self.wx,self.wy, coord)
-                self.validation(cx,cy)
+                if self.image_num==-1:
+                    if self.use_dataloader:
+                        self.validation_dl(cx,cy)
+                    else:
+                        self.validation(cx,cy)
+                else:
+                    self.validation_im(cx,cy,self.image_num)
+
                 if (k % self.save_rate)==0 or k==(total-1):
                     info = {'loss':self.lossmat}
                     
@@ -274,8 +276,41 @@ class LandscapeVis():
                     
                     info['computed']=self.computedmat
                     scipy.io.savemat(self.folders['surface_path']+"/"+self.surface_name+".mat", info) 
+        
+        info = {'loss':self.lossmat}
+                    
+        for key,value in self.metrics_eval.items():
+            info[key]=self.metricsmat[key]
+                    
+        info['computed']=self.computedmat
+        scipy.io.savemat(self.folders['surface_path']+"/"+self.surface_name+".mat", info) 
+        self.visdom_plot()
 
 
+    def validation_im(self,cx,cy,i): 
+        ttime=time.time()  
+        
+        print('Computing {} {}:'.format(cx,cy), end=' ')
+        lendata=len(self.traindataset)
+        with torch.no_grad():    
+            sample=self.traindataset[i % lendata]
+            sample = self.warp_var_mod.warp_Variable(sample,self.device)
+            for v in sample.keys():
+                if isinstance(sample[v],torch.Tensor):
+                    sample[v].unsqueeze_(0)
+            images=sample['image']
+            
+            outputs = self.net(images)
+            kwarg=eval(self.losseval)
+            loss=self.criterion(**kwarg)
+            self.lossmat[cx,cy]+=loss.item()
+            for key,value in self.metrics_eval.items():
+                kwarg=eval(self.metrics_eval[key])
+                metric=self.metrics[key](**kwarg)
+                self.metricsmat[key][cx,cy]+=metric.item()
+
+        self.computedmat[cx,cy]=1
+        print(time.time()-ttime)
 
     def validation(self,cx,cy): 
         ttime=time.time()  
@@ -290,6 +325,36 @@ class LandscapeVis():
                 for v in sample.keys():
                     if isinstance(sample[v],torch.Tensor):
                         sample[v].unsqueeze_(0)
+                images=sample['image']
+                
+
+                outputs = self.net(images)
+                kwarg=eval(self.losseval)
+                loss=self.criterion(**kwarg)
+                self.lossmat[cx,cy]+=loss.item()
+
+                for key,value in self.metrics_eval.items():
+                    kwarg=eval(self.metrics_eval[key])
+                    metric=self.metrics[key](**kwarg)
+                    self.metricsmat[key][cx,cy]+=metric.item()
+                
+                if (i+1)==self.batch_size:
+                    break
+        
+        self.lossmat[cx,cy]/= i+1
+        for key,value in self.metrics_eval.items():
+            self.metricsmat[key][cx,cy]/=i+1
+
+        self.computedmat[cx,cy]=1
+        print(time.time()-ttime)
+        
+    def validation_dl(self,cx,cy): 
+        ttime=time.time()  
+        
+        print('Computing {} {}:'.format(cx,cy), end=' ')
+        with torch.no_grad():    
+            for i,sample in enumerate(self.train_loader):
+                sample = self.warp_var_mod.warp_Variable(sample,self.device)
                 images=sample['image']
                 
 
@@ -327,19 +392,26 @@ class LandscapeVis():
 
     def loadmodel(self,net,modelpath):
         if os.path.isfile(modelpath):
+            netx=copy.deepcopy(net)
+
             checkpoint = torch.load(modelpath,map_location='cpu')
-            to_load= net.module if self.use_parallel else net
+            to_load= netx.module if self.use_parallel else netx
+            if 'weights' in checkpoint.keys():
+                return checkpoint['weights']
             to_load.load_state_dict(checkpoint['net'])
+
+            return create_target_direction(net,netx)
         else:
             raise Exception('Model not found')
 
-    def savemodel(self,net,modelpath=''):
+    def savemodel(self,net,weights,modelpath=''):
         if modelpath=='':
             print('Saving checkpoint  {} {}\n'.format(self.cx,self.cy))
             modelpath=os.path.join(self.folders['surface_path'],'model{}_{}.t7'.format(self.cx,self.cy))
         to_save= net.module if self.use_parallel else net
         state = {
                 'arch':  self.arch,
-                'net':   to_save.state_dict()
+                'net':   to_save.state_dict(),
+                'weights': weights
             }
         torch.save(state, modelpath)

utils/landscape_utils.py
@@ -39,6 +39,14 @@ def set_states(net, states, dx=None, dy=None, step=None):
         v.add_(d.type(v.type()))
     net.load_state_dict(new_states)
 
+def get_random_states(states):
+    """
+        Produce a random direction that is a list of random Gaussian tensors
+        with the same shape as the network's state_dict(), so one direction entry
+        per weight, including BN's running_mean/var.
+    """
+    return [torch.randn(w.size()) for k, w in states.items()]
+
 ################################################################################
 #                        Normalization Functions
 ################################################################################
@@ -69,7 +77,7 @@ def normalize_direction(direction, weights, norm='filter'):
 
 def normalize_directions_for_states(direction, states, norm='filter', ignore='biasbn'):
     assert(len(direction) == len(states))
-    for (_,d), (k, w) in zip(direction.items(), states.items()):
+    for d, (k, w) in zip(direction, states.items()):
         if d.dim() <= 1:
             if ignore == 'biasbn':
                 d.fill_(0) # ignore directions for weights with 1 dimension